4b54f9d31908fea0eec04e870621fc4c68e8ff9e
jppinedal <54005283+jppinedal@users.noreply.github.com>
2022-03-22 23:38:52 -0500

    motor

--- src/main/java/Motor.java
+++ src/main/java/Motor.java
@@ -1,17 +1,17 @@
-public class Motor {
+package test;
+public class Motor{
     int numeroCilindros;
     String tipo;
     int registro;
-
+    
     void cambiarRegistro(int registro){
         this.registro = registro;
-
     }
     void asignarTipo(String tipo){
-        if (tipo == "electrico" || tipo == "gasolina"){
-            this.tipo = tipo;
-        }
-
-
+    	switch (tipo) {
+    	case "electrico": this.tipo=tipo; 
+    	case "gasolina": this.tipo=tipo; 
+    	}
     }
+
 }
