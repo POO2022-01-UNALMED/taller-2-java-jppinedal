70c6358e14d452b64ccf27124504c0e07c5c29fe
jppinedal <54005283+jppinedal@users.noreply.github.com>
2022-03-22 23:35:56 -0500

    Corrección

--- src/main/java/Motor.java
+++ src/main/java/Motor.java
@@ -1,16 +1,17 @@
-package test;
-public class Motor{
+public class Motor {
     int numeroCilindros;
     String tipo;
     int registro;
+
     void cambiarRegistro(int registro){
         this.registro = registro;
+
     }
     void asignarTipo(String tipo){
-    	switch (tipo) {
-    	case "electrico": this.tipo=tipo; break;
-    	case "gasolina": this.tipo=tipo; break;
-    	}
-    }
+        if (tipo == "electrico" || tipo == "gasolina"){
+            this.tipo = tipo;
+        }
 
+
+    }
 }
