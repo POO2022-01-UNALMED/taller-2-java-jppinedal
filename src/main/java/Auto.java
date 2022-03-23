43c8e17aaa8f4d9109695b362d42f8e1db150b7f
jppinedal <54005283+jppinedal@users.noreply.github.com>
2022-03-22 22:21:38 -0500

    Se adjunta archivos en Java

---
+++ src/main/java/Auto.java
@@ -0,0 +1,32 @@
+package test;
+public class Auto {
+    String modelo;
+    int precio;
+    Asiento[] asientos;
+    Motor motor;
+    int registro;
+    static int cantidadCreados;
+    cantidadCreados cantidadAsientos(Asiento[] asientos){
+        cantidadCreados=asientos.length;
+        int i = 0;
+        while (asientos.length > 0) {
+        	if (asientos[i]==null) {
+        		cantidadCreados--;
+        		i++;
+        	}
+        }   
+        return cantidadCreados;
+    }
+    String verificarIntegridad(){
+    	int j=0;
+    	while(asientos.length>0) {
+    		if ((this.asientos[j].registro==this.motor.registro) && (this.asientos[j].registro==this.registro) && (this.motor.registro==this.registro)){
+    			j++;
+                return "Auto original";
+            }else {
+            	j++;
+            	return "Las piezas no son originales";
+            }
+    	}
+    }
+}
